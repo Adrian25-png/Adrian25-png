c24f2c2bc8fcbfae12d331383d94e6919b63c322
AdrianSC <deividadrian123@gmail.com>
2026-03-10 13:26:44 -0300

    Add loader functionality on window load

---
+++ script.js
@@ -0,0 +1,15 @@
+window.addEventListener("load", function(){
+
+const loader = document.querySelector(".loader")
+
+setTimeout(()=>{
+
+loader.style.opacity = "0"
+
+setTimeout(()=>{
+loader.style.display = "none"
+},500)
+
+},1500)
+
+})
